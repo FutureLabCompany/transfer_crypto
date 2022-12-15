b69127b24cf9426fe8543666c17d64ec169cd9e8
Omid Beheshtian <beheshtian.omid82@gmail.com>
2022-12-15 16:42:25 +0330

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,8 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutliineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
 const Navbar = () => {
     return (
         <h1>
