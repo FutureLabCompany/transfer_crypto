ff79c144614867910a674428bb88e5a89f7ce8da
Omid Beheshtian <beheshtian.omid82@gmail.com>
2022-12-15 16:58:59 +0330

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import logo from '../../images/logo.png';
 
 const Navbar = () => {
     return (
-        <h1>
-            Navbar
-        </h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            
+        </nav>
     );
 }
 
