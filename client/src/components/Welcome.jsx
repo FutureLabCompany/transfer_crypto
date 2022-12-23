1b3ac048bb425c09fe5e76609cb7ffd90c4df775
Omid Beheshtian <beheshtian.omid82@gmail.com>
2022-12-24 00:00:43 +0330

    import needed for Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,3 +1,9 @@
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+
 const Welcome = () => {
     return (
         <h1>
